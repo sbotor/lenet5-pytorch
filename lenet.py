dcf9f912f455ec289605383e57da436ac0c64e48
Szymon <botorszym@gmail.com>
2023-06-02 22:41:55 +0200

    Add training class, improve model persistence

--- lenet.py
+++ lenet.py
@@ -3,9 +3,7 @@ import torch
 
 _NUM_CLASSES = 10
 
-
 class LeNet5(nn.Module):
-
     def __init__(self):
         super().__init__()
 
@@ -17,37 +15,45 @@ class LeNet5(nn.Module):
         self.fc2 = nn.Linear(in_features=84, out_features=_NUM_CLASSES)
 
         self.pool = nn.AvgPool2d(kernel_size=2)
-        self.tanh = nn.Tanh()
-        self.softmax = nn.Softmax(dim=_NUM_CLASSES)
+        self.activ = nn.Tanh()
 
-        self._epochs_trained = 0
-
-    @property
-    def epochs_trained(self):
-        return self._epochs_trained
-
-    def mark_epoch(self):
-        self._epochs_trained += 1
+        self.epoch = 0
 
     def forward(self, x: torch.Tensor):
-        x = self.tanh(self.conv1(x))
+        x = self.activ(self.conv1(x))
         x = self.pool(x)
-        x = self.tanh(self.conv2(x))
+        x = self.activ(self.conv2(x))
         x = self.pool(x)
-        x = self.tanh(self.conv3(x))
+        x = self.activ(self.conv3(x))
 
         x = x.reshape(x.shape[0], -1)
-        x = self.tanh(self.fc1(x))
-        x = self.softmax(self.fc2(x))  # Not sure
+        x = self.activ(self.fc1(x))
+        x = self.activ(self.fc2(x))
 
         return x
 
-    def save(self, path: str):
-        torch.save(self.state_dict(), path)
+    @staticmethod
+    def save(path: str, model: 'LeNet5', optimizer, loss_history):
+        state = {
+            'model_state': model.state_dict(),
+            'optimizer_state': optimizer.state_dict(),
+            'loss_history': loss_history,
+            'epoch': model.epoch
+        }
+
+        torch.save(state, path)
 
     @staticmethod
     def load(path: str):
+        state = torch.load(path)
         model = LeNet5()
-        model.load_state_dict(torch.load(path))
 
-        return model
+        model.load_state_dict(state['model_state'])
+        model.epoch = state['epoch']
+        
+        optimizer = torch.optim.Adam(model.parameters())
+        optimizer.load_state_dict(state['optimizer_state'])
+
+        loss_history = state['loss_history']
+
+        return model, optimizer, loss_history
--- main.py
+++ main.py
@@ -1,5 +1,34 @@
+from lenet import LeNet5
+from utils import Trainer, load_trainer
+import torch
+
+LEARNING_RATE = 0.0001
+
+def create_and_train():
+    model = LeNet5()
+    optim = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    trainer = Trainer(model, optim)
+
+    trainer.train(1)
+
+    LeNet5.save('model.pt', model, optim, trainer.loss_history)
+
+def load_and_train():
+    trainer = load_trainer('model.pt')
+
+    trainer.train(10)
+
+    LeNet5.save('model.pt', trainer.model, trainer.optimizer, trainer.loss_history)
+
+def print_data():
+    model, _, loss_history = LeNet5.load('model.pt')
+
+    print(f'Epoch: {model.epoch}')
+    print(f'Loss history: {loss_history}')
+
 def main():
-    pass
+    load_and_train()
+    print_data()
 
 
 if __name__ == '__main__':
